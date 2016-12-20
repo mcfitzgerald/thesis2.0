a2b770b9c1f4b0f62f57540d17a1a7e314a02e95
mcfitzgerald <michael.craig.fitzgerald@gmail.com>
2016-12-20 07:12:09 -0500

    birddog

---
+++ ipythes/allnbs/models.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+#define an objective function for use with lmfit (must mesh with lmfit.Parameter class)
+def wyman_lmfit(parm,lig,data,rtot,eps=None):
+    """maximizes vector ops"""
+    k11 = parm['k11']
+    k21 = parm['k21']
+    k22 = parm['k22']
+    l20 = parm['l20']
+    
+    ligc = np.concatenate(lig)
+    datac = np.concatenate(data)
+    
+    rfree = (((-1 - k11*ligc)) + \
+    (np.sqrt(np.concatenate((np.square((1 + k11*lig)) + \
+    8.*l20*rtot*(1 + k21*lig + k21*k22*(np.square(lig)))))))) \
+    / (4*l20*(1 + k21*ligc + k21*k22*(np.square(ligc))))    
+       
+    bfrac = (k11*ligc + l20*k21*rfree*ligc + \
+    2*l20*k21*k22*rfree*(np.square(ligc))) \
+    / (1 + 2*l20*rfree + k11*ligc + \
+    2*l20*k21*rfree*ligc + 2*l20*k21*k22*rfree*(np.square(ligc)))
+    
+    residual = (bfrac - datac)
+    
+    if eps is None:
+        return residual
+    else:
+        weights = 1/(np.concatenate(eps))
+        return (residual*weights)
+    
+#define an objective function for use with scipy optimize (obviates parameter class)
+#eps is standard deviation
+def wym_rtot_sp(rtot,lig,data,parm,eps=None):
+    k11 = parm[0]
+    k21 = parm[1]
+    k22 = parm[2]
+    l20 = parm[3]
+    
+    ligc = np.concatenate(lig)
+    datac = np.concatenate(data)
+    
+    rfree = (((-1 - k11*ligc)) + \
+    (np.sqrt(np.concatenate((np.square((1 + k11*lig)) + \
+    8.*l20*rtot*(1 + k21*lig + k21*k22*(np.square(lig)))))))) \
+    / (4*l20*(1 + k21*ligc + k21*k22*(np.square(ligc))))    
+       
+    bfrac = (k11*ligc + l20*k21*rfree*ligc + \
+    2*l20*k21*k22*rfree*(np.square(ligc))) \
+    / (1 + 2*l20*rfree + k11*ligc + \
+    2*l20*k21*rfree*ligc + 2*l20*k21*k22*rfree*(np.square(ligc)))
+    
+    residual = (bfrac - datac)
+
+    if eps is None:
+        return residual
+    else:
+        weights = 1/(np.concatenate(eps))
+        return (residual*weights)
