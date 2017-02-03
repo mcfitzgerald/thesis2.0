2bffcce6d6608a8fdee4a7f1caa92fa6debb124e
mcfitzgerald <michael.craig.fitzgerald@gmail.com>
2017-02-02 21:37:03 -0500

    crystal power

--- ipythes/working_jan17/simulated_sets/mods/models.py
+++ ipythes/working_jan17/simulated_sets/mods/models.py
@@ -31,7 +31,10 @@ def wyman_lmfit(parm,lig,data,rtot,eps=None):
         return (residual*weights)
 
 #define an objective function for use with scipy optimize (obviates parameter class)
-#eps is standard deviation
+#eps is standard deviation. *NOTE* this works only when the data is an irregular
+#array, i.e., a 1D'array of arrays' that forms when arrays of different sizes are 
+#used to construct an array. This allows proper 'broadcasting'. A modified function
+#is included below
 def wyman_sp(parm,lig,data,rtot,eps=None):
     k11 = parm[0]
     k21 = parm[1]
@@ -89,7 +92,7 @@ def wym_rtot_sp(rtot,lig,data,parm,eps=None):
         return (residual*weights)
     
 #define an objective function for use with scipy optimize (obviates parameter class)
-#weights are just the fitted y data. *NOTE* this only works with 1dim arrays
+#weights are just the fitted y data.
 def wyman_sp_relwt(parm,lig,data,rtot):
     k11 = parm[0]
     k21 = parm[1]
@@ -142,3 +145,33 @@ def wyman_bestfit(parm,lig,rtot):
     2*l20*k21*rfree*ligc + 2*l20*k21*k22*rfree*(np.square(ligc)))
     
     return bfrac
+
+#MODIFIED FUNCTION FOR USE WITH 'PERFECT' RECTANGULAR DATA ARRAYS
+#SUCH AS THOSE FROM SIMULATION
+def wyman_sp_rect(parm,lig,data,rtot,eps=None):
+    k11 = parm[0]
+    k21 = parm[1]
+    k22 = parm[2]
+    l20 = parm[3]
+    
+    ligc = np.concatenate(lig)
+    datac = np.concatenate(data)
+    rtot_tr = rtot[:,None]
+    
+    rfree = (((-1 - k11*ligc)) + \
+    (np.sqrt(np.concatenate((np.square((1 + k11*lig)) + \
+    8.*l20*rtot_tr*(1 + k21*lig + k21*k22*(np.square(lig)))))))) \
+    / (4*l20*(1 + k21*ligc + k21*k22*(np.square(ligc))))    
+       
+    bfrac = (k11*ligc + l20*k21*rfree*ligc + \
+    2*l20*k21*k22*rfree*(np.square(ligc))) \
+    / (1 + 2*l20*rfree + k11*ligc + \
+    2*l20*k21*rfree*ligc + 2*l20*k21*k22*rfree*(np.square(ligc)))
+    
+    residual = (bfrac - datac)
+
+    if eps is None:
+        return residual
+    else:
+        weights = 1/(np.concatenate(eps))
+        return (residual*weights)
